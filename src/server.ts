ac1c5a7ab8662ced271e715c048b7df222479c5e
Lara Portilho <lara.portilho.m@gmail.com>
2021-06-22 18:30:09 -0300

    Adicionado o banco de dados, repositories, entities e services

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import 'reflect-metadata';
+import { router } from './routes';
+import './database';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+app.listen(3000, () => console.log("Server is running at http://localhost:3000"));
